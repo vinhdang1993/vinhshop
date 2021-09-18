c7431e5c959319c40dbad1fe6cdd75f32cce173a
vinhdang1993 <41432372+vinhdang1993@users.noreply.github.com>
2021-09-18 16:38:41 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 // import express from ('express')
-var cors = require('cors')
+const cors = require('cors')
 const dotenv = require('dotenv')
 const route = require('./routes/index.js')
 const db = require('./config/db');
 const bodyParser = require('body-parser');
 dotenv.config();
 db.connectDB();
-
+const PORT = process.env.PORT || 5000
 const app = express()
-app.use(cors())
+const corsOptions ={
+    origin:'*', 
+    credentials:true,          
+    optionSuccessStatus:200,
+ }
+ app.use(cors(corsOptions))
 
 app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json()); 
@@ -19,6 +24,6 @@ app.use(bodyParser.json());
 //     })
 //   )
 route(app);
-const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT,console.log(`http://localhost:${PORT}`) )
